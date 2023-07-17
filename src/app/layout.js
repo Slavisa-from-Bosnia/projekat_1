00c000212b4aa486ec3a7cf1f4aa494c126f470a
Slavisa Babic <babic.slavisa@gmail.com>
2023-07-17 22:10:12 +0200

    auth

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import './globals.css'
 import { Inter, Roboto, Poppins } from 'next/font/google'
 import Footer from '@/components/footer/Footer'
 import { ThemeProvider } from '@/context/ThemeContext'
+import AuthProvider from '@/components/AuthProvider/AuthProvider'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -15,13 +16,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ThemeProvider>
-        <div className='container'>
-          <Navbar/>
-            {children}
-          <Footer/>
-        </div>
-        </ThemeProvider>
+      <ThemeProvider>
+        <AuthProvider>
+          <div className='container'>
+            <Navbar/>
+              {children}
+            <Footer/>
+          </div>
+        </AuthProvider>
+      </ThemeProvider>
       </body>
     </html>
   )
